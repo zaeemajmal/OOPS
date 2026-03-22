3d51f62f6e3b11ea5ee2937d318db0f65930e2e2
zaeemajmal <ajzaeem.md@gmail.com>
2026-03-22 16:56:32 +0530

    Add files via upload

---
+++ dog breed.py
@@ -0,0 +1,10 @@
+class dog:
+    def __init__(self,name,price):
+        self.name = name
+        self.price = price
+ob = dog("French Bulldog",20000)
+ob2 = dog("Golden retriever",3000)
+print(ob.name)
+print(ob.price)     
+print(ob2.name)
+print(ob2.price)  
